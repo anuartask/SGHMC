ab9158807dfab792c049faacd6ecaaeb8680a884
anuartask <taskynov.anuar@mail.ru>
2017-10-16 01:39:03 +0300

    fixed bugs

--- sghmc.py
+++ sghmc.py
@@ -2,11 +2,13 @@ import numpy as np
 
 class SGHMC(object):
     
-    def __init__(self, grad_objective,
+    def __init__(self, grad_likelihood,
+                 grad_prior,
                 eta=1e-4, batch_size=100,
                 epochs=10, seed=None, 
                  alpha=0.01):
-        self.grad_objective = grad_objective
+        self.grad_likelihood = grad_likelihood
+        self.grad_prior = grad_prior
         self.eta = eta
         self.batch_size = batch_size
         self.epochs = epochs
@@ -14,13 +16,21 @@ class SGHMC(object):
         self.alpha = alpha
         self.noise_var = np.sqrt(2 * eta * alpha)
     
+    def _grad_objective(self, X, w, y=None):
+        if y is not None:
+            res = self._batch_counts * self.grad_likelihood(X, w, y) + self.grad_prior(w)
+            return res / np.linalg.norm(res)
+        else:
+            res = self._batch_counts * self.grad_likelihood(X, w) + self.grad_prior(w)
+            return res / np.linalg.norm(res)
+    
     def samples_return(self, X, w_init=None, y=None, display=False):
         
         np.random.seed(self.seed)
         self.num_objects, num_features = X.shape
         self.batch_counts = self.num_objects // self.batch_size
         self._batch_counts = self.num_objects / self.batch_size
-        indices = np.arange(num_objects)
+        indices = np.arange(self.num_objects)
         iterations_cnt = self.batch_counts * self.epochs
         self.samples = np.zeros((iterations_cnt, num_features))
         noise = self.noise_var * np.random.randn(iterations_cnt, num_features)
@@ -41,19 +51,12 @@ class SGHMC(object):
                     y_batch = y[batch_ind]
                 else:
                     y_batch = None
-                if y is not None:
-                    new_w = self._w - self._batch_counts * self.eta * self.grad_objective(self._w, X_batch, y_batch) +\
-                            (1. - self.alpha) * self.velocity +\
-                            noise[i, :]
+                new_w = self._w - self.eta * self._grad_objective(X_batch, self._w, y=y_batch) +\
+                        (1. - self.alpha) * self.velocity +\
+                        noise[i, :]
                     
-                    self.velocity = new_w - self._w
-                    self._w = new_w.copy()
-                else:
-                    new_w = self._w - self._batch_counts * self.eta * self.grad_objective(self._w, X_batch) +\
-                            (1. - self.alpha) * self.velocity +\
-                            noise[i, :]
-                    self.velocity = new_w - self._w
-                    self._w = new_w.copy()
+                self.velocity = new_w - self._w
+                self._w = new_w.copy()
                 
                 self.samples[i, :] = self._w
             if display and (epoch % 100 == 0):
@@ -62,18 +65,29 @@ class SGHMC(object):
 
 class SGNHT(object):    
     def __init__(self,
-                 grad_objective,
+                 grad_likelihood,
+                 grad_prior,
                  eta=0.01, batch_size=100,
                  epochs=10, seed=None, 
                  a=0):
-        self.grad_objective = grad_objective
+        self.grad_likelihood = grad_likelihood
+        self.grad_prior = grad_prior
         self.eta = eta
         self.epochs = epochs
-        self.alpha = a
-        self.noise_var = np.sqrt(2 * eta * a)
+        self.a = a
+        self.alpha = self.a
+        self.noise_var = np.sqrt(2 * self.eta * self.a)
         self.batch_size = batch_size
         self.seed = seed
         self.epochs = epochs
+    
+    def _grad_objective(self, X, w, y=None):
+        if y is not None:
+            res = self._batch_counts * self.grad_likelihood(X, w, y) + self.grad_prior(w)
+            return res# / np.linalg.norm(res)
+        else:
+            res = self._batch_counts * self.grad_likelihood(X, w) + self.grad_prior(w)
+            return res# / np.linalg.norm(res)
     
     def samples_return(self, X, w_init=None, y=None, display=False):
         np.random.seed(self.seed)
@@ -81,12 +95,11 @@ class SGNHT(object):
         self.batch_counts = self.num_objects // self.batch_size
         self._batch_counts = self.num_objects / self.batch_size
         
-        indices = np.arange(num_objects)
+        indices = np.arange(self.num_objects)
         iterations_cnt = self.batch_counts * self.epochs
         self.samples = np.zeros((iterations_cnt, num_features))
         noise = self.noise_var * np.random.randn(iterations_cnt, num_features)
-        self.velocity = np.zeros(num_features)
-        
+        self.velocity = np.sqrt(self.eta) * np.random.randn(num_features)
         self.alphas = []
         
         if w_init is not None:
@@ -104,24 +117,13 @@ class SGNHT(object):
                 else:
                     y_batch = None
                 i = epoch * self.batch_counts + batch
-                
-                if y is not None:
-                    new_w = self._w - self.eta * self._batch_counts * self.grad_objective(self._w, X_batch, y_batch) +\
-                            (1. - self.alpha) * self.velocity +\
-                            noise[i, :]
-                    self.velocity = new_w - self._w
-                    self._w = new_w.copy()
-                    self.alphas.append(self.alpha)
-                    self.alpha += 1. / num_features * (self.velocity ** 2).sum() - self.eta
-                else:
-                    new_w = self._w - self.eta * self._batch_counts * self.grad_objective(self._w, X_batch) +\
-                            (1. - self.alpha) * self.velocity +\
-                            noise[i, :]
-                    self.velocity = new_w - self._w
-                    self._w = new_w.copy()
-                    self.alphas.append(self.alpha)
-                    self.alpha += 1. / num_features * (self.velocity ** 2).sum() - self.eta
-                
+                new_w = self._w - self.eta * self._grad_objective(X_batch, self._w, y=y_batch) +\
+                        (1. - self.alpha) * self.velocity +\
+                        noise[i, :]
+                self.velocity = new_w - self._w
+                self._w = new_w.copy()
+                self.alphas.append(self.alpha)
+                self.alpha += 1. / num_features * (self.velocity ** 2).sum() - self.eta
                 self.samples[i, :] = self._w
             if display and (epoch % 100 == 0):
                 print(epoch)                          
